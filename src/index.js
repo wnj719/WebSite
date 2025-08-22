84946ef6c725e9133924e98f1227e33f91c413b2
wnj719 <fina79513@gmail.com>
2025-08-22 16:16:25 +0900

    배포 수정

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/WebSite'>
+  <BrowserRouter basename={Process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
   
